c35f9d3eba1c65fe92b1cd00ba7920e5985fb32e
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-09-11 14:33:04 -0300

    application updated

--- aplication/main.py
+++ aplication/main.py
@@ -1,17 +1,27 @@
-from IA_stock_exchange_project.aplication.get.get_data import get_stock_data
-from IA_stock_exchange_project.aplication.output.plot_data import plot_stock_data
+from get.get_data import get_stock_data
+from output.plot_data import plot_stock_data
+
+def main():
+    # Símbolo para B3SA3
+
+    symbol = "B3SA3.SA"  # Símbolo correto para yfinance
+
+    # Obtenha os dados
+    df = get_stock_data(symbol)
+    df = df[df.index.dayofweek < 5]  # Remover finais de semana
+    df = df.between_time('09:00', '17:30') # Filtrar para o horário de 9:00 até 17:30
+    df
+
+    # Verifique se os dados foram obtidos com sucesso antes de tentar plotar
+    if df is not None and not df.empty:
+        # Gere o gráfico
+        plot_stock_data(df)
+    else:
+        print("Não foi possível obter os dados do ativo.")
 
 
-# Símbolo para B3SA3
 
-symbol = "B3SA3.SA"  # Símbolo correto para yfinance
 
-# Obtenha os dados
-df = get_stock_data(symbol)
 
-# Verifique se os dados foram obtidos com sucesso antes de tentar plotar
-if df is not None and not df.empty:
-    # Gere o gráfico
-    plot_stock_data(df)
-else:
-    print("Não foi possível obter os dados do ativo.")
+if __name__ == "__main__":
+    main()  
--- aplication/output/plot_data.py
+++ aplication/output/plot_data.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 # Função para plotar gráfico de linha
 def plot_stock_data(df):
     plt.figure(figsize=(12, 6))
-    plt.plot(df.index, df['Close'], label='Preço de Fechamento')
+    plt.plot(df.index, df['Close'], label='Preço de Fechamento', alpha=0.7)
+    plt.plot(df.index, df['Open'], label='Preço de Abertura', alpha=0.2)
     plt.title('B3SA3 - Preço por Hora (Última Semana)')
     plt.xlabel('Data e Hora')
     plt.ylabel('Preço de Fechamento (R$)')
